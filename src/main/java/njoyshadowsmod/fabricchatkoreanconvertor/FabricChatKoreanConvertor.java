f2283f8782d51735817cc6171ff4fe05cced90ed
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2021-12-29 11:56:47 +0900

    add command

--- src/main/java/njoyshadowsmod/fabricchatkoreanconvertor/FabricChatKoreanConvertor.java
+++ src/main/java/njoyshadowsmod/fabricchatkoreanconvertor/FabricChatKoreanConvertor.java
@@ -2,6 +2,7 @@ package njoyshadowsmod.fabricchatkoreanconvertor;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.text.Text;
 import njoyshadowsmod.fabricchatkoreanconvertor.utils.EnKData;
 import njoyshadowsmod.fabricchatkoreanconvertor.utils.UUidUtil;
 
@@ -17,18 +18,26 @@ public class FabricChatKoreanConvertor implements ModInitializer {
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("enk").executes(context -> {
+
                 UUID playerUUID = context.getSource().getPlayer().getUuid();
                 boolean IsExist = false;
-                boolean IsTwice = false;
+
                 for (EnKData playerdata: UUidUtil.playerList) {
                     if(playerdata.getUUID() == playerUUID){
                         IsExist = true;
+
+                        playerdata.setIsEnable(!playerdata.getIsEnable());
+                        context.getSource().getPlayer().sendSystemMessage(Text.of(String.format("EnK is %b", playerdata.getIsEnable())),playerUUID);
                         break;
                     }
                 }
+
                 if(!IsExist){
                     UUidUtil.playerList.add(new EnKData(playerUUID,true));
+                    context.getSource().getPlayer().sendSystemMessage(Text.of(String.format("EnK is %s", "register")),playerUUID);
+
                 }
+
                 return 1;
             }));
         });
